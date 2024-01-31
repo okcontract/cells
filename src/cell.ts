c08d402acee5d895def943984e4798019c2708c2
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-01-31 18:25:52 +0800

    cleanup: log messages

--- src/cell.ts
+++ src/cell.ts
@@ -581,7 +581,7 @@ export class Cell<
    * @returns mapped cell
    */
   map = <T, NF extends boolean = false>(
-    fn: (v: V) => T | Promise<T | AnyCell<T>> | AnyCell<T>,
+    fn: (v: V, prev?: T) => T | Promise<T | AnyCell<T>> | AnyCell<T>,
     name?: string,
     noFail?: NF
   ): MapCell<T, NF> =>
