f6e058ec25c572613d09e4024a1093d6eff57ced
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-02-29 12:05:41 -0700

    array: rewrite filter, improve gc use, find

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,6 +1,17 @@
 import { expect, test } from "vitest";
 
-import { filter, mapArray, reduce, sort } from "./array";
+import type { Graph } from "@okcontract/graph";
+
+import {
+  filter,
+  filterPredicateCell,
+  findIndex,
+  mapArray,
+  mapArrayCell,
+  reduce,
+  sort
+} from "./array";
+import type { AnyCell } from "./cell";
 import { _uncellify } from "./cellify";
 import { delayed } from "./promise";
 import { SheetProxy } from "./proxy";
@@ -37,6 +48,27 @@ test("mapArray", async () => {
   await expect(_uncellify(m)).resolves.toEqual([5, 4, 6]);
   expect(await (await m.get())[2].get()).toBe(6);
   expect(sheet.stats.count).toBe(10); // @unchanged
+});
+
+test("mapArrayCell function change", async () => {
+  const sheet = new Sheet();
+  const proxy = new SheetProxy(sheet);
+  const l = proxy.new([1, 2, 3].map((v) => proxy.new(v)));
+  expect(sheet.stats.count).toBe(4);
+  const fn = proxy.new((v: AnyCell<number>) => v.map((v) => v + 1));
+  const m = mapArrayCell(proxy, l, fn);
+  expect(sheet.stats.count).toBe(9); // +4 cells
+  await expect(_uncellify(m)).resolves.toEqual([2, 3, 4]);
+
+  // update one cell
+  (await l.get())[0].set(4);
+  await expect(_uncellify(m)).resolves.toEqual([5, 3, 4]);
+  expect(sheet.stats.count).toBe(9); // @unchanged
+
+  // function change
+  fn.set((v: AnyCell<number>) => v.map((v) => v - 1));
+  await expect(_uncellify(m)).resolves.toEqual([3, 1, 2]);
+  expect(sheet.stats.count).toBe(12); // +3 new mapped cells
 });
 
 test("mapArray async", async () => {
@@ -94,6 +126,29 @@ test("reduce array", async () => {
   expect(sheet.stats.count).toBe(8); // +1 cell +1 pointer
 });
 
+test("function equality", () => {
+  const f = (v: number) => v + 1;
+  const g = (v: number) => v + 1;
+  // expect(f == g).toBe(false);
+  expect(f === g).toBe(false);
+  // expect(f == f).toBe(true);
+  // expect(f === f).toBe(true);
+  const m = new Map();
+  m.set(f, "a");
+  expect(m.get(f)).toBe("a");
+  expect(m.get(g)).toBeUndefined();
+
+  const sheet = new Sheet();
+  const proxy = new SheetProxy(sheet);
+
+  const a = proxy.new(1);
+  const b = a.map(f);
+
+  // @ts-expect-error private
+  const gr = sheet.g as Graph<number>;
+  expect(gr.get(a.id)).toEqual([b.id]);
+});
+
 test("sort array", async () => {
   const sheet = new Sheet();
   const proxy = new SheetProxy(sheet);
@@ -123,13 +178,46 @@ test("filter array", async () => {
   const l = proxy.new([1, 5, 3].map((v) => proxy.new(v)));
   expect(sheet.stats.count).toBe(4); // 3+1 array
 
-  filter(l, (v) => v > 2); // no await
+  const pred = proxy.new((v: number) => v > 2);
+  const f = filter(proxy, pred, l, "threshold", true); // no await
+  expect(sheet.stats.count).toBe(7); // + pred + filter + pointer
 
-  // but we wait before expecting
+  // we wait before expecting
   await proxy.working.wait();
-  await expect(_uncellify(l)).resolves.toEqual([5, 3]);
+  await expect(_uncellify(f)).resolves.toEqual([5, 3]);
   // the removed cell is not deleted ("garbage collected" at proxy level)
-  expect(sheet.stats.count).toBe(4); // unchanged
+  expect(sheet.stats.count).toBe(7); // unchanged
+
+  // update one cell
+  (await l.get())[0].set(4);
+  await expect(_uncellify(f)).resolves.toEqual([4, 5, 3]);
+  expect(sheet.stats.count).toBe(7); // unchanged
+
+  // change predicate
+  pred.set((v) => v > 3);
+  await expect(_uncellify(f)).resolves.toEqual([4, 5]);
+  expect(sheet.stats).toEqual({ size: 7, count: 8 }); // one pointer updated
+});
+
+test("filterPredicateCell array", async () => {
+  const sheet = new Sheet();
+  const proxy = new SheetProxy(sheet);
+
+  const l = proxy.new([1, 5, 3].map((v) => proxy.new(v)));
+  expect(sheet.stats).toEqual({ size: 4, count: 4 }); // 3+1 array
+
+  const pred = proxy.new((v: AnyCell<number>) => v.map((_v) => _v > 2));
+  const f = filterPredicateCell(proxy, pred, l, "threshold", true); // no await
+  expect(sheet.stats).toEqual({ size: 11, count: 11 }); // +3+1 pred output array +1 pointer +1 filtered
+
+  // we wait before expecting
+  await proxy.working.wait();
+  await expect(_uncellify(f)).resolves.toEqual([5, 3]);
+
+  pred.set((v: AnyCell<number>) => v.map((_v) => _v > 3));
+  await proxy.working.wait();
+  await expect(_uncellify(f)).resolves.toEqual([5]);
+  expect(sheet.stats).toEqual({ size: 14, count: 15 }); // we should not recreate mapped cells
 });
 
 test("sorted array pointer", async () => {
@@ -141,6 +229,15 @@ test("sorted array pointer", async () => {
   // not required
   // await proxy.working.wait();
   await expect(count.get()).resolves.toBe(4);
+});
+
+test("findIndex", async () => {
+  const proxy = new Sheet().newProxy();
+  const l = proxy.new([1, 5, 3].map((v) => proxy.new(v)));
+  const idx = findIndex(proxy, l, (v) => v === 5);
+  await expect(idx.get()).resolves.toBe(1);
+  l.update((l) => [proxy.new(1), ...l]);
+  await expect(idx.get()).resolves.toBe(2);
 });
 
 test("sort array remapped", async () => {
@@ -165,12 +262,12 @@ test("sort array remapped", async () => {
   (await l.get())[0].set(4);
   await expect(_uncellify(s)).resolves.toEqual([3, 4, 5]);
   await expect(sum.get()).resolves.toBe(12);
-  expect(sheet.stats).toEqual({ size: 9, count: 10 }); // replace sorted pointer + sum pointer
+  expect(sheet.stats).toEqual({ size: 9, count: 10 }); // size unchanged, one pointer changed
 
   // add one cell
   l.update((arr) => [...arr, proxy.new(1)]);
   await expect(_uncellify(l)).resolves.toEqual([4, 5, 3, 1]);
   await expect(_uncellify(s)).resolves.toEqual([1, 3, 4, 5]);
   await expect(sum.get()).resolves.toBe(13);
-  expect(sheet.stats).toEqual({ size: 10, count: 13 }); // +1 original cell, replace sorted pointer + sum pointer
+  expect(sheet.stats).toEqual({ size: 10, count: 13 }); // +1 original cell, changed 2 pointers
 });
--- src/array.ts
+++ src/array.ts
@@ -1,5 +1,5 @@
-import type { AnyCell, MapCell, ValueCell } from "./cell";
-import { filterAsync } from "./filter-async";
+import { type AnyCell, MapCell, ValueCell } from "./cell";
+import { collector, reuseOrCreate } from "./gc";
 import type { SheetProxy } from "./proxy";
 
 export type CellArray<T> = AnyCell<AnyCell<T>[]>;
@@ -18,19 +18,28 @@ export type CellArray<T> = AnyCell<AnyCell<T>[]>;
 export const mapArray = <T, U>(
   proxy: SheetProxy,
   arr: CellArray<T>,
-  fn: (v: T, index?: number) => U | Promise<U>
+  fn: (v: T, index?: number) => U | Promise<U>,
+  name = "map"
 ): MapCell<MapCell<U, false>[], false> =>
   proxy.map(
     [arr],
-    (cells, prev) =>
-      cells.map(
-        (cell, index) =>
-          // reuse previously mapped cell
-          prev?.find((_c) => _c.dependencies?.[0] === cell.id) ||
+    (cells, prev) => {
+      const set = new Set((prev || []).map((cell) => cell.id));
+      const res = cells.map((cell, index) => {
+        // reuse previously mapped cell
+        const reuse = prev?.find((_c) => _c.dependencies?.[0] === cell.id);
+        if (reuse !== undefined) set.delete(reuse.id);
+        return (
+          reuse ||
           // create new map
-          proxy.map([cell], (_cell) => fn(_cell, index))
-      ),
-    "map"
+          proxy.map([cell], (_cell) => fn(_cell, index), `[${index}]`)
+        );
+      });
+      // collect unused previously mapped cells
+      proxy._sheet.collect(...[...set]);
+      return res;
+    },
+    name
   );
 
 /**
@@ -72,33 +81,74 @@ export const sort = <T>(
  * @param fn
  * @returns mapped array cell
  */
-export const mapArrayCell = <T, U>(
+export const mapArrayCell = <T, U, NF extends boolean = false>(
   proxy: SheetProxy,
   arr: CellArray<T>,
-  fn: (v: AnyCell<T>) => U | Promise<U>
-): MapCell<MapCell<U, false>[], false> =>
-  proxy.map([arr], (cells, prev) =>
-    cells.map(
-      (cell) =>
-        // reuse previously mapped cell
-        prev?.find((_c) => _c.dependencies?.[0] === cell.id) ||
-        // create new map
-        proxy.map([cell], (_) => fn(cell))
-    )
+  fn: AnyCell<(v: AnyCell<T>, idx?: number) => AnyCell<U>>,
+  name = "map",
+  nf?: NF
+): MapCell<MapCell<U, NF>[], NF> => {
+  let prevFn: (v: AnyCell<T>) => AnyCell<U>;
+  return proxy.map(
+    [arr, fn],
+    (cells, _fn, prev) => {
+      const arr = cells.map((cell, i) =>
+        // @todo collect previously mapped cells
+        reuseOrCreate(
+          _fn === prevFn, // function unchanged
+          () => prev?.find((_c) => _c.dependencies?.[0] === cell.id), // reuse
+          () => _fn(cell, i) as MapCell<U, NF> // create new map
+        )
+      );
+      prevFn = _fn;
+      return arr;
+    },
+    name,
+    nf
   );
+};
 
 /**
- * collector returns a function that automatically collects a cell
- * each time it is re-created.
+ * first element of recursive arrays.
  */
-export const collector = <C extends AnyCell<unknown>>(proxy: SheetProxy) => {
-  let prev: C;
-  return (v: C) => {
-    // mark the previous value for deletion
-    if (prev) proxy._sheet.collect(prev);
-    prev = v;
-    return prev;
-  };
+export const first = <T>(
+  proxy: SheetProxy,
+  arr: AnyCell<T | AnyCell<T>[]>,
+  name = "last"
+): MapCell<T, boolean> => {
+  // @todo collectors (on path?)
+  const aux = (arr: AnyCell<T | AnyCell<T>[]>) =>
+    proxy.mapNoPrevious(
+      [arr],
+      (_arr) => {
+        if (!Array.isArray(_arr)) return arr as AnyCell<T>;
+        if (_arr.length === 0) return null; // @todo pointer to nullCell?
+        return aux(_arr[0]);
+      },
+      name
+    );
+  return aux(arr);
+};
+
+/**
+ * last element of recursive arrays.
+ */
+export const last = <T>(
+  proxy: SheetProxy,
+  arr: AnyCell<T | AnyCell<T>[]>,
+  name = "last"
+): MapCell<T, boolean> => {
+  // @todo collectors (on path?)
+  const aux = (arr: AnyCell<T | AnyCell<T>[]>) =>
+    proxy.mapNoPrevious(
+      [arr],
+      (_arr) => {
+        if (Array.isArray(_arr)) return aux(_arr[_arr.length - 1]);
+        return arr as AnyCell<T>;
+      },
+      name
+    );
+  return aux(arr);
 };
 
 /**
@@ -137,22 +187,177 @@ export const reduce = <
   );
 };
 
+export const find = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  arr: CellArray<T>,
+  predicate: (v: T) => boolean,
+  name = "find",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<T, NF>>(proxy);
+  return proxy.map(
+    [arr],
+    (cells) =>
+      coll(proxy.mapNoPrevious(cells, (..._cells) => _cells.find(predicate))),
+    name,
+    nf
+  );
+};
+
+// @todo generalize
+export const findCell = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  arr: CellArray<T>,
+  predicate: AnyCell<(v: AnyCell<T>) => AnyCell<boolean>>,
+  name = "find",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<T, NF>>(proxy);
+  // Since predicate is a reactive function, we have to instantiate
+  // the computation for each cell.
+  // const keep = mapArrayCell(proxy, arr, predicate, "keep", nf);
+  let prevFn: (elt: AnyCell<T>) => AnyCell<boolean>;
+  const keep = proxy.map(
+    [predicate, arr],
+    (fn, cells, _prev: AnyCell<boolean>[]) => {
+      // console.log({ keep: cells.length });
+      // @todo if the predicate function has changed, collect previous mapped cells
+      const keep = cells.map((cell) => {
+        // We can reuse a cell only if the predicate hasn't changed.
+        // @todo collect previously mapped cells for deleted cells in arr
+        const reuse =
+          prevFn === fn &&
+          _prev?.find((_c) => _c.dependencies?.[0] === cell.id);
+        return reuse || fn(cell);
+      });
+      prevFn = fn;
+      return keep;
+    },
+    "keep",
+    nf
+  );
+  return proxy.map(
+    [arr, keep],
+    (cells, _keep) =>
+      coll(
+        proxy.mapNoPrevious(_keep, (..._flat) => cells.find((_, i) => _flat[i]))
+      ),
+    name,
+    nf
+  );
+};
+
+export const findIndex = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  arr: CellArray<T>,
+  fn: (v: T) => boolean,
+  name = "find",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<number, NF>>(proxy);
+  return proxy.map(
+    [arr],
+    (cells) =>
+      coll(proxy.mapNoPrevious(cells, (..._cells) => _cells.findIndex(fn))),
+    name,
+    nf
+  );
+};
+
 /**
  * filter updates a cellified array in a `ValueCell` using a predicate function as filter.
  * @param arr
  * @param predicate function that returns `true` for kept values.
  * @returns nothing
  * @description filtered out cells are _not_ deleted
- * @todo not consistent
  */
-export const filter = <T>(
-  arr: ValueCell<AnyCell<T>[]>,
-  predicate: (elt: T) => boolean
-) =>
-  arr.update(async (items) =>
-    filterAsync(items, async (item) => {
-      const v = await item.get();
-      // we keep errors
-      return v instanceof Error || predicate(v);
-    })
+export const filter = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  predicate: AnyCell<(elt: T) => boolean>,
+  arr: CellArray<T>,
+  name = "filter",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<AnyCell<T>[], NF>>(proxy);
+  return proxy.map(
+    [predicate, arr],
+    (fn, cells) =>
+      coll(
+        proxy.mapNoPrevious(
+          cells,
+          (..._cells) => cells.filter((_, i) => fn(_cells[i])),
+          "_filter"
+        )
+      ),
+    name,
+    nf
   );
+};
+
+export const mapFlat = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  arr: CellArray<T>,
+  name = "flatten",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<T[], NF>>(proxy);
+  return proxy.map(
+    [arr],
+    (cells) => coll(proxy.mapNoPrevious(cells, (..._cells) => _cells)),
+    name,
+    nf
+  );
+};
+
+/**
+ * filterPredicateCell updates a cellified array in a `ValueCell` using a predicate function as filter.
+ * @param arr
+ * @param predicate as Cell function that returns `true` for kept values.
+ * @returns nothing
+ * @description filtered out cells are _not_ deleted
+ */
+export const filterPredicateCell = <T, NF extends boolean = false>(
+  proxy: SheetProxy,
+  predicate: AnyCell<(elt: AnyCell<T>) => AnyCell<boolean>>,
+  arr: CellArray<T>,
+  name = "filter",
+  nf?: NF
+) => {
+  const coll = collector<MapCell<AnyCell<T>[], NF>>(proxy);
+  // Since predicate is a reactive function, we have to instantiate
+  // the computation for each cell.
+  // const keep = mapArrayCell(proxy, arr, predicate, "keep", nf);
+  let prevFn: (elt: AnyCell<T>) => AnyCell<boolean>;
+  const keep = proxy.map(
+    [predicate, arr],
+    (fn, cells, _prev: AnyCell<boolean>[]) => {
+      // console.log({ keep: cells.length });
+      // @todo if the predicate function has changed, collect previous mapped cells
+      const keep = cells.map((cell) => {
+        // We can reuse a cell only if the predicate hasn't changed.
+        // @todo collect previously mapped cells for deleted cells in arr
+        const reuse =
+          prevFn === fn &&
+          _prev?.find((_c) => _c.dependencies?.[0] === cell.id);
+        return reuse || fn(cell);
+      });
+      prevFn = fn;
+      return keep;
+    },
+    "keep",
+    nf
+  );
+  return proxy.map(
+    [arr, keep],
+    (cells, _keep) =>
+      coll(
+        proxy.mapNoPrevious(
+          _keep,
+          (..._flat) => cells.filter((_, i) => _flat[i]),
+          "filter.map"
+        )
+      ),
+    name,
+    nf
+  );
+};
