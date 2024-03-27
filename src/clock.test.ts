fd8c3a970ab2721dde6aaf7d562c380547f16f69
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-03-27 15:45:31 +0800

    clock: wait list test

--- src/clock.test.ts
+++ src/clock.test.ts
@@ -39,3 +39,32 @@ test("clockWork", async () => {
   await sleep(15);
   await expect(work.get()).resolves.toBe(1001);
 });
+
+type M = Record<string, number>;
+test("wait list", async () => {
+  const sheet = new Sheet();
+  const proxy = new SheetProxy(sheet);
+  const live = proxy.new(true);
+  const cl = clock(proxy, live, 10);
+  const q = proxy.new([] as string[], "q");
+  const m = clockWork(
+    proxy,
+    cl,
+    [q],
+    (_q: string[], prev) =>
+      ({
+        ...((prev as M) || ({} as M)),
+        ...Object.fromEntries(_q.map((elt) => [elt, elt.length]))
+      }) as M,
+    "m"
+  );
+  await expect(m.get()).resolves.toEqual({});
+  q.set(["foo", "bar"]);
+  await expect(m.get()).resolves.toEqual({});
+  await sleep(15);
+  await expect(m.get()).resolves.toEqual({ foo: 3, bar: 3 });
+  q.set(["test"]);
+  await expect(m.get()).resolves.toEqual({ foo: 3, bar: 3 });
+  await sleep(10);
+  await expect(m.get()).resolves.toEqual({ foo: 3, bar: 3, test: 4 });
+});
