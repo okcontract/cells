e87e16406472c5fe59e845d233f0d7c7f184ed9a
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-05-15 20:19:20 +0800

    initial: new helper

---
+++ src/initial.ts
@@ -0,0 +1,19 @@
+import type { AnyCell, MapCell } from "./cell";
+import type { SheetProxy } from "./proxy";
+
+export const initialValue = <T>(
+  proxy: SheetProxy,
+  v0: T,
+  v: AnyCell<T>,
+  name = "initial"
+): MapCell<T, true> => {
+  const cell = proxy.new(v0, name);
+  v.subscribe((v) => {
+    // We do not propagate errors yet.
+    if (v instanceof Error) return;
+    cell.set(v);
+  });
+  // We fake being a MapCell to prevent setting the cell
+  // outside of this function.
+  return cell as unknown as MapCell<T, true>;
+};
