2bb4fd5b74a176883de8e9bdddd64fe8af5998f8
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-05-26 15:01:57 +0800

    debouncer: add test

---
+++ src/debouncer.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+
+import { debouncer } from "./debouncer";
+import { isEqual } from "./isEqual.test";
+import { sleep } from "./promise";
+import { Sheet } from "./sheet";
+
+test("debouncer", async () => {
+  const proxy = new Sheet(isEqual).newProxy();
+  const waiting = proxy.new(false);
+  const deb = debouncer(20, waiting);
+  const v = proxy.new(0);
+  for (let i = 1; i <= 10; i++) {
+    deb((i) => v.set(i), i);
+    await sleep(5);
+    expect(v.consolidatedValue).toBe(0);
+  }
+  await sleep(30);
+  expect(v.consolidatedValue).toBe(10);
+});
