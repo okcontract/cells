3379b77fe557d9ed86144cb23ad84e82292ac91e
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2025-12-15 21:50:09 +0800

    vitest: setup, overriding Node new mock

---
+++ vitest-setup.ts
@@ -0,0 +1,22 @@
+import { vi } from "vitest";
+
+// Disable Node.js's localStorage if it's being injected
+delete globalThis.localStorage;
+
+const localStorageMock = (() => {
+  let store: { [key: string]: string } = {};
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value.toString();
+    },
+    clear: () => {
+      store = {};
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+  };
+})();
+
+vi.stubGlobal("localStorage", localStorageMock);
